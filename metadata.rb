9332b8ae4715cd895cf8026d75188f947f957910
Erik Michael Purins <erik.purins@imgtec.com>
2014-08-18 15:07:42 -0700

    Switch maintainer of fork. Disable broken cookbooks in test fixtures.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name             'jenkins'
-maintainer       'Chef Software, Inc.'
-maintainer_email 'releng@getchef.com'
+name             'epu-cb-jenkins'
+maintainer       'Erik Purins'
+maintainer_email 'erik@purins.com'
 license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
