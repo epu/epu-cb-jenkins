20f5abb29c04f8f8a2cd32590f2881b85531ed36
Erik Michael Purins <erik.purins@imgtec.com>
2015-05-26 10:27:44 -0700

    Ubuntu 15.04/debian 80 - update apt constraint and version number.

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'erik@purins.com'
 license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.1.5'
+version          '2.1.6'
 
 recipe 'master', 'Installs a Jenkins master'
 
-depends 'apt',   '~> 2.0'
+depends 'apt',   '~> 2.7'
 depends 'gdebi', '~> 1.1'
 depends 'runit', '~> 1.5'
 depends 'yum',   '~> 3.0'
